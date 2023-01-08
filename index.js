744870b572234893216068f3cdf9ec1c29400c9d
Michael Kiros <michael.kiros00@gmail.com>
2023-01-08 17:25:56 -0500

    commit tree eee08b65d95baf2561bf0c31b68cbd2f66c7e2f9

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+main.remove(main)
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "<h1>victory</h1><p>Michael is the champion</p>"
